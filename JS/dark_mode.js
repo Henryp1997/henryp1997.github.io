00e56e619d354305fb3427732093bf5429b62315
Henry Pickersgill <htap2@live.co.uk>
2024-09-29 22:50:06 +0100

    fix dark mode switch

--- JS/dark_mode.js
+++ JS/dark_mode.js
@@ -11,10 +11,14 @@ function toggle_dark_mode () {
 
 function toggle_light () {
     document.getElementById("pagestyle").setAttribute("href", "./assets/light.css");
-    localStorage.setItem("dark_mode", "./assets/light.css")
+    localStorage.setItem("dark_mode", "./assets/light.css");
+    const toggleCircle = document.getElementById('toggleCircle');
+    toggleCircle.style.transform = 'translateX(0)';
 }
 
 function toggle_dark () {
     document.getElementById("pagestyle").setAttribute("href", "./assets/dark.css");
-    localStorage.setItem("dark_mode", "./assets/dark.css")
+    localStorage.setItem("dark_mode", "./assets/dark.css");
+    const toggleCircle = document.getElementById('toggleCircle');
+    toggleCircle.style.transform = 'translateX(20px)';
 }
