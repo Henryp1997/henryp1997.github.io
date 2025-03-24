28a45fefede9587861c8a3459a625c50b5ce1b57
Henry.Pickersgill <henry.pickersgill@forefrontrf.com>
2025-03-24 17:36:49 +0000

    redo darts checkbox image selection code - much simpler binary approach to filenames

--- JS/darts_checkboxes.js
+++ JS/darts_checkboxes.js
@@ -1,70 +1,17 @@
 function darts_checkbox(elem) {
     target = elem.id.split("_")[0];
     all_checkboxes = [`${target}_first_dart`, `${target}_second_dart`, `${target}_third_dart`, `${target}_singles`, `${target}_doubles`, `${target}_trebles`];
-    checked = [false, false, false, false, false, false];
+    checked = [0, 0, 0, 0, 0, 0];
     for (i in all_checkboxes) {
         if (document.getElementById(all_checkboxes[i]).checked) {
-            checked[i] = true;
+            checked[i] = 1;
         }
     }
-    
-    file = `./assets/darts/${target}/score_freq_dart_`
+    file = `./assets/darts/${target}/`
 
-    // check cases for dart 1 box ticked
-    if (checked[0]) {
-        if (checked[1]) {
-            if (checked[2]) {
-                file += "all"
-            }
-            else {
-                file += "1_and_2"
-            }
-        }
-        else {
-            if (checked[2]) {
-                file += "1_and_3"
-            }
-            else {
-                file += "1"
-            }
-        }
-    }
-    else {
-        // check cases for dart 2 and 3 box ticked
-        if (checked[1]) {
-            if (checked[2]) {
-                file += "2_and_3"
-            }
-            else {
-                file += "2"
-            }
-        }
-        else if (checked[2]) {
-            file += "3"
-        }
-    }
+    // Use checkbox values to point to correct graph image
+    file += `${checked[0]}${checked[1]}${checked[2]}${checked[3]}${checked[4]}${checked[5]}.svg`;
 
-    // now check cases for singles box ticked
-    if (checked[3]) {
-        file += "_sngl"
-        if (checked[4]) {
-            file += "_dbl"
-        }
-        if (checked[5]) {
-            file += "_tbl"
-        }
-    }
-    else if (checked[4]) {
-        file += "_dbl"
-        if (checked[5]) {
-            file += "_tbl"
-        }
-    }
-    else {
-        file += "_tbl"
-    }
-
-    file += ".svg";
     document.getElementById(`${target}_image_wrapper`).href = file;
     document.getElementById(`${target}_image`).src = file
 }
